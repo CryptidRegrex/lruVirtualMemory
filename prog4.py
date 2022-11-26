f12a261f1c6c452ff34f0d1779ac6052b673b176
CryptidRegrex <65179166+CryptidRegrex@users.noreply.github.com>
2022-11-26 13:40:10 -0600

    Comments have been added, program tested and old testing print statements removed.

--- prog4.py
+++ prog4.py
@@ -3,7 +3,12 @@ import sys
 hit = 0
 miss = 0
 
-
+"""
+This is the main function that takes parameters of the file to compute hit rate for four processes. 
+The input should look like such: python3 prog4 file 5 5 5 5 
+Each value you enter is the size of the page table for the process
+This will take arguments when initializing the program via the system command line. 
+"""
 def main():
     totalHit = 0
     allPages = 0
@@ -60,7 +65,14 @@ def main():
     #print(miss)
     exit(0)
 
-
+"""
+This process readsIn a file and compiles a list of all pages to process in order of acending order
+Params:
+file - This is the input file object that will be read in and parsed for each procees.
+process - This is the integer value of the process that we are parsing into a list of pages
+returns:
+localQueue - This is the list of all pages that the process will use
+"""
 def readIn(file, process):
     fullQueue = []
     with open(file, 'r') as f:
@@ -72,180 +84,192 @@ def readIn(file, process):
                 fullQueue.append(int(element[1]))
     return fullQueue
 
-
+"""
+addLRU process the first value for each process and puts it into a list
+Params:
+fullQueue - all items in the process queue
+localQueue - all local queue items being that of the maximum page table size
+page - the page we are going to append to the page table
+qSize - the maximum page table size
+returns:
+localQueue - the page table with the first entry in it
+"""
 def addLRU(fullQueue, localQueue, page, qSize):
     if (len(localQueue) == 0):
         localQueue.append(str(fullQueue[0]) + ',' + str(qSize))
     return localQueue
-    """
-    count = 0
-    localQueue.append()
-    for i in localQueue:
 
-        lru = parseLRU(i)
-
-        #localQueue.pop(count)
-        count += 1
-    
-    if (len(localQueue) < qSize):
-        print(f"newVal {newVal}")
-        element = str(localQueue[newVal]) + ',' + str(5)
-        localQueue.append(element)
-    return element
-    """
-
+"""
+missPage does all of the calculations for adding a new page and calculating new lru values for each page
+in the table
+Params:
+fullQueue - all items in the process queue
+localQueue - all local queue items being that of the maximum page table size
+index - the value of the item we need to take from fullQueue and add to localQueue
+qSize - the maximum page table size
+returns:
+retLocalQueue - This returns the localQueue after a missPage value has been calculated
+"""
 def missPage(fullQueue, localQueue, qSize, index):
     global miss
     miss += 1
-    #print(f"SIZE of our localQueue at the beginning of missPage function: {len(localQueue)}")
-    #print(fullQueue[index])
     retLocalQueue = []
     count = 0
     if (len(localQueue) == 0):
-        #localQueue.append(fullQueue[index])
         page = fullQueue[0]
         retLocalQueue = addLRU(fullQueue, localQueue, page, qSize)
-        #print(f"When local queue is 0 our value is {localQueue[0]}")
-        #print(f"q size in miss: {qSize}")
     elif (len(localQueue) > 0 and (len(localQueue) < qSize)):
-        #print(f"Our localQueue len is: {len(localQueue)}")
-        #for i in localQueue:
-            #print(f"What are we about to look through in missPage before the queue is full? {i}")
         for i in localQueue:
-            #print(f"Our value being analyized in missPage is {i}")
             newLru = parseLRU(i) - 1
             retainPage = parsePage(i)
-            #localQueue.pop(count)
             retLocalQueue.append(createTableEntry(retainPage, newLru))
-            #count += 1
         retLocalQueue.append(createTableEntry(fullQueue[index], qSize))
-        #print(f"After our elif statement our retlocalQueue is size: {len(retLocalQueue)}")
-        #print(f"Still filling array {localQueue[(len(localQueue) - 1)]}")
     else:
         for i in localQueue:
-            #for x in localQueue:
-                #print(f"Our values in the localQueue before checking if lru == 1 {x}")
-            #print(f"ELSE statement in missPage function size of local queue {len(retLocalQueue)}")
             if (parseLRU(i) == 1):
-                #print("Are we hitting missPage once full?")
-                #print(parseLRU(i))
                 localQueue.pop(count)
                 retLocalQueue = updateLRU(localQueue, qSize)
                 retLocalQueue.append(createTableEntry(fullQueue[index], qSize))
-                #print(f"ELSE statement in missPage function size of local queue {len(retLocalQueue)}")
                 break
             count += 1
-        #for x in retLocalQueue:
-            #print(f"Our values when returning from missPage ELSE: {x}")    
-    #print(f"When returning our localQueue in missPage function the size is: {len(retLocalQueue)}")
     return retLocalQueue
 
-
+"""
+This function creates an entry into the page table with an lru value
+Params:
+page - this is the page we are going to add to the table
+lru - this is the value we are appending to the entry
+returns:
+element - This is the table entry we are going to append to our list
+"""
 def createTableEntry(page, lru):
     element = str(page) + ',' + str(lru)
     return element
 
+"""
+This funciton computers the new lru values if we hit a page in a page table
+Params:
+localQueue - This is the page table we are going to update lru values for on a page hit
+qSize - This is the maximum size of the page table we are using
+index - This is the locaiton of the page we hit in the page table for processing
+returns:
+localQueue - This is an updated queue with new lru values based on the page hit
+"""
 def hitPage(localQueue, qSize, index):
     global hit 
-    #hit += 1
     retLocalQueue = []
     count = 0
     maxVal = qSize
     lru = parseLRU(localQueue[index])
-    #print(f"Index {index} In hitpage Page {parsePage(localQueue[index])} lru {parseLRU(localQueue[index])}")
     pageHit = parsePage(localQueue[index])
     localQueue.pop(index)
-    #for i in localQueue:
-        #print(f"After popping the value we have these in the localQ {i} and our len of localQ is {len(localQueue)}")
     totalVals = len(localQueue)
     while count < totalVals:
-    #for i in localQueue:
         if (lru < parseLRU(localQueue[count])):
             page = parsePage(localQueue[count])
             newlru = parseLRU(localQueue[count]) - 1
-            #print(f"page {page} new lru {lru}")
-            #print(f"localQueue with count before pop {localQueue[count]}")
-            #localQueue.pop(count)
-            #localQueue.append(createTableEntry(page, newlru))
             retLocalQueue.append(createTableEntry(page, newlru))
         else:
             retLocalQueue.append(localQueue[count])
         count += 1
     retLocalQueue.append(createTableEntry(pageHit, maxVal))
     localQueue = retLocalQueue
-    #for i in localQueue:
-        #print(f"After hitPage before return {i}")
-    #print(f"When returning our localQueue in hitPage function the size is: {len(localQueue)}")
     return localQueue
     
-
+"""
+Due to how we are storing our values in our list with each element being a page then lru value this 
+will parse the page out of the element
+Params:
+element - This is the element we will disect for the page
+returns:
+page - This is the integer of the page
+"""
 def parsePage(element):
     item = element.split(',')
     page = int(item[0])
     return page
 
-
+"""
+Due to how we are storing our values in our list with each element being a page then lru value this 
+will parse the lru value out of the element
+Params:
+element - This is the element we will disect for the lru value
+returns:
+lru - the integer value of the lru
+"""
 def parseLRU(element):
     item = str(element).split(',')
     lru = int(item[1])
     return lru
 
+
+"""
+This function updates the values in a full page table when a missPage occurs. Each element's lru will be
+negated by one.
+Params:
+localQueue - The page table we are using to update our lru values
+returns:
+updatedQ - the page table with updated lru values
+"""
 def updateLRU(localQueue, qSize):
     updatedQ = []
     for i in localQueue:
         lru = parseLRU(i) - 1
         page = parsePage(i)
         updatedQ.append(createTableEntry(page, lru))
-    #print(f"When returning our localQueue in updateLRU function the size is: {len(localQueue)}")
     return updatedQ
-    #parseLRU()
-    #for page in localQueue:
-        #parseLRU()
 
+
+"""
+This function will check for a hit or miss when analyizing the next page to load into the table.
+If it hits it will call the hitPage() function and if it misses it will call the missPage() function
+Params:
+fullQueue - The entire list of pages the process will need to load
+localQueue - The page table we are using to retain in use or used pages
+qSize - The maximum size of the page table
+index - the location of the next page in the fullQueue
+returns:
+localQueue - The new updated page table after a hitPage() or missPage()
+"""
 def checkHit(fullQueue, localQueue, qSize, index):
     count = 0
     found = False
     if (len(localQueue) > 0):
         for val in localQueue:
-            #print(f"localQueue page {parsePage(val)} fullQueue page {fullQueue[index]}")
             if parsePage(val) == fullQueue[index]:
                 global hit
                 hit += 1
-                #print(f"The size of our localQueue before a hitPage call is: {len(localQueue)}")
                 localQueue = hitPage(localQueue, qSize, count)
-                #print(f"WE FOUND OUR VALUE | localQueue page {parsePage(val)} fullQueue page {fullQueue[index]}")
-                #print(f"count in checkHit {count}")
                 found = True
                 break
             count += 1
-        #print(found)
         if (found == False):
             localQueue = missPage(fullQueue, localQueue, qSize, index)
     elif (len(localQueue) == 0):
-        #addLRU(fullQueue, localQueue, count, qSize)
         localQueue = missPage(fullQueue, localQueue, qSize, count)
-        #print(localQueue[0])
-    #for i in localQueue:
-        #print(f"After checkHit before return {i}")
-    #print("checkHit is finished")
     return localQueue
                 
                 
-
+"""
+This function will complete all processing of pages that are provided by the input file for a specific process
+It will check for a hit every new page that is loaded from the queue
+Params:
+queue - This is the full queue with every page that will need to be loaded in the life of the process
+qSize - This is the maximum page table size for the process
+returns:
+None
+"""
 def completeProcess(queue, qSize):
     totalLoads = len(queue)
     count = 0
     localQueue = []
     while True:
         if (count < totalLoads):
-            #print(f"len value for localQueue {len(localQueue)}")
-            #print(count)
             localQueue = checkHit(queue, localQueue, qSize, count)
-            #print(f"{localQueue[count]}")
             count += 1
         else:
             break
 
 
-
-main()
+if __name__ == "__main__":
+    main()
